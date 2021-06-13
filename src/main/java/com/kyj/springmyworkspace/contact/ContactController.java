4685fb8c9200225b65063f01e771073cd22fa9f6
Kim youngjae <kyjjdata@gmail.com>
2021-06-14 01:21:49 +0900

    paging 추가

--- src/main/java/com/kyj/springmyworkspace/contact/ContactController.java
+++ src/main/java/com/kyj/springmyworkspace/contact/ContactController.java
@@ -7,6 +7,8 @@ import java.util.Optional;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +18,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -32,6 +35,11 @@ public class ContactController {
 	public List<Contact> getContactList() {
 		// SELECT * FROM contact ORDER BY id DESC; -- 역순(descending) 정렬
 		return repo.findAll(Sort.by("id").descending());
+	}
+
+	@GetMapping(value = "/contacts/paging")
+	public Page<Contact> getContactListPaging(@RequestParam int page, @RequestParam int size) {
+		return repo.findAll(PageRequest.of(page, size, Sort.by("id").descending()));
 	}
 
 	@PostMapping(value = "/contacts")
